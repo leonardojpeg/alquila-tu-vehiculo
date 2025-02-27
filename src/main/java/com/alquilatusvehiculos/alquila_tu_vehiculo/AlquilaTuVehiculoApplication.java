b0d79ea5f3689e3c466affdae08493c9b83516a0
leonardojpeg <lcuevass@uoc.edu>
2025-02-27 21:32:47 +0100

    Primer commit - Proyecto base

---
+++ src/main/java/com/alquilatusvehiculos/alquila_tu_vehiculo/AlquilaTuVehiculoApplication.java
@@ -0,0 +1,13 @@
+package com.alquilatusvehiculos.alquila_tu_vehiculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilaTuVehiculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilaTuVehiculoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alquilatusvehiculos/alquila_tu_vehiculo/AlquilaTuVehiculoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alquilatusvehiculos.alquila_tu_vehiculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilaTuVehiculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
